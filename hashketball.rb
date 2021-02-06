e7176e01fbf0746b0fff303cc1dde671fb8a20c7
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-06 01:50:49 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -5,8 +5,8 @@ require 'pry'
 def game_hash
   {
     :home => {
-      :team_name => "",
-      :colors => [""],
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => [
         {
           player_name: "Alan Anderson",
@@ -66,8 +66,8 @@ def game_hash
       ]
     },
     :away => {
-      :team_name => "",
-      :colors => [""],
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
       :players =>[
         {
           player_name: "Jeff Adrien",
